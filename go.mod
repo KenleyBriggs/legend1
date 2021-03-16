6aaa6bf0892b078f811f461e19c35b1724deac61
kenleybriggs <kenleybriggs@icloud.com>
2021-03-15 20:08:57 -0600

    Original commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/briggs/test
+
+go 1.16
+
+replace github.com/briggs/myfunc => ../myfunc
+
+require github.com/briggs/myfunc v0.0.0-00010101000000-000000000000
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/briggs/myfunc"
+)
+
+
+
+func main() {
+	fmt.Println("Legend Pricing: ")
+
+	three := 1000000
+	six := 2000000
+	ten := 30000000
+	legend.LegendPricing(three, six, ten)
+
+	}
+	//sum := myfunc.Add2(numbers)
+	//fmt.Println(sum)
+
+
+
