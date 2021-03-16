364c2bd37af40b752c7a77c8872fc3c57948bb44
JeffNaef <jeff@triggermesh.com>
2021-03-16 08:00:58 -0700

    creates basic server implementation

--- go.mod
+++ go.mod
@@ -5,3 +5,10 @@ go 1.16
 //replace github.com/briggs/myfunc => ../myfunc
 
 //require github.com/briggs/myfunc v0.0.0-00010101000000-000000000000
+
+require (
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/prometheus/common v0.19.0
+	go.uber.org/zap v1.16.0
+	knative.dev/pkg v0.0.0-20210315160101-6a33a1ab29ac
+)
--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/prometheus/common/log"
+	"go.uber.org/zap"
+	"knative.dev/pkg/logging"
+)
+
+// Receiver q
+type Receiver struct {
+	httpC http.Client
+
+	logger *zap.SugaredLogger
+}
+
+// Request is the structure of the event we expect to receive.
+type Request struct {
+	ID          int64  `json:"id"`
+	Description string `json:"description"`
+	Tag         string `json:"tag"`
+}
+
 func main() {
+	ctx := context.Background()
+	r := Receiver{}
+	r.logger = logging.FromContext(ctx)
+	if err := envconfig.Process("", &r); err != nil {
+		log.Fatal(err)
+	}
 
+	log.Info("Starting the HTTP server now..")
+	http.HandleFunc("/", r.getPricing)
+	http.ListenAndServe(":8080", nil)
+}
+
+func (recv *Receiver) getPricing(w http.ResponseWriter, r *http.Request) {
 	MetalLegendPricing(1000, 2000, 3000)
-
 }
